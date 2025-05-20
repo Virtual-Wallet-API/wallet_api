e749b72426fceee13bf1491cb956f329fb16015f
fcgith <steliyan.slavov31@icloud.com>
2025-05-20 19:11:13 +0300

    validators, setup and requirements

---
+++ app/infrestructure/validators.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException
+
+
+def validate_username(v: str) -> str:
+    # Check if username is between 2 and 20 characters long
+    if len(v) < 2 or len(v) > 20:
+        raise HTTPException(status_code=400, detail="Username must be between 2 and 20 characters long")
+
+    return v
+
+
+def validate_password(v: str):
+    # Check if password is at least 8 characters long
+    if len(v) < 8:
+        raise HTTPException(status_code=400,
+                            detail="Password must be at least 8 characters long")
+    # Check if password contains at least one digit, one uppercase letter and one special character
+    if not any(char.isdigit() for char in v) \
+            or not any(char.isupper() for char in v) \
+            or all(char.isalnum() for char in v):
+        raise HTTPException(status_code=400,
+                            detail="Password must contain at least one digit, one uppercase letter and one special character")
+    return v
+
+def validate_phone_number(v: int):
+    if len(str(v)) != 10:
+        raise HTTPException(status_code=400, detail="Phone number must be valid")
+    return v
