0ba0300652ff2b78449fdfd52dc0a5893168368d
gosho359 <yovchev1909@gmail.com>
2025-05-21 13:58:53 +0300

    Contact Schema created

---
+++ app/schemas/contact.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+from pydantic_core.core_schema import field_after_validator_function
+
+
+class ContactBase(BaseModel):
+    contact_id: int
+
+class ContactCreate(ContactBase):
+    user_id: int
+
+    @field_validator("contact_id")
+    def validate_not_self(cls, contact_id, values):
+        user_id = values.get("user_id")
+        if user_id == contact_id:
+            raise ValueError("You cannot add yourself as a contact")
+        return contact_id
+
+    class Config:
+        from_attributes = True
