99811065d977be1e869d544529ad8adff68d6011
fcgith <steliyan.slavov31@icloud.com>
2025-05-21 18:14:43 +0300

    fe

--- frontend/routers/root.py
+++ frontend/routers/root.py
@@ -21,6 +21,30 @@ def read_overview(request: Request):
     return templates.TemplateResponse("overview.html", data)
 
 
+@router.get("/cards", response_model=None)
+def read_cards(request: Request):
+    data = {"request": request,
+            "page": "cards"}
+
+    dcards = [
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "purple"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "blue", "status":"terminated"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "green"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "green", "status":"terminated"},
+    ]
+
+    ccards = [
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "red"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "red"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "blue"},
+        {"last_four":1234, "expiry": "01/23", "balance": "1000.00$", "color": "purple"},
+    ]
+
+    data["debit_cards"] = dcards
+    data["credit_cards"] = ccards
+    return templates.TemplateResponse("cards.html", data)
+
+
 @router.get("/transactions", response_model=None)
 def read_transactions(request: Request):
     data = {"request": request,
@@ -28,6 +52,6 @@ def read_transactions(request: Request):
     return templates.TemplateResponse("payments/transactions.html", data)
 
 
-@router.get("/logout", response_model=None)
-def read_logout(request: Request):
-    return RedirectResponse("/")
+# @router.get("/logout", response_model=None)
+# def read_logout(request: Request):
+#     return RedirectResponse("/")
