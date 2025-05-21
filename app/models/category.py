df4bd29873c037f7b95b5a65f4334535dee7b0c3
Martin <martin.kitukov@gmail.com>
2025-05-21 13:38:43 +0300

    category model

---
+++ app/models/category.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, Enum, Float, String
+from sqlalchemy.orm import relationship
+from app.infrestructure import Base
+from fastapi import HTTPException
+from sqlalchemy.orm import validates
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    transactions = relationship("Transaction", back_populates="category")
+
+    @validates("name")
+    def validate_name(self, key, v: str):
+        if len(v) < 2:
+            raise HTTPException(status_code=400,
+                                detail="Name must be at least 2 characters long")
+        return v
