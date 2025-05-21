99e5e221a2521dab47b04aadee77b2499c7ab58d
Martin <martin.kitukov@gmail.com>
2025-05-21 13:38:51 +0300

    transaction model

---
+++ app/models/transaction.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, Enum, Float
+from sqlalchemy.orm import relationship
+from app.infrestructure import Base
+from fastapi import HTTPException
+from sqlalchemy.orm import validates
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    receiver_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    amount = Column(Float, nullable=False)
+    date = Column(DateTime, nullable=False)
+    status = Column(Enum("pending", "completed", "failed", name="status"), nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+
+    category = relationship("Category", back_populates="transactions")
+
+    @validates("amount")
+    def validate_amount(self, key, v: float):
+        if v < 0:
+            raise HTTPException(status_code=400,
+                                detail="Amount must be positive")
+        return v
+
