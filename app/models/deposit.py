17c20702ba942ffd1cfcfbb5863c901d3945e042
Martin <martin.kitukov@gmail.com>
2025-05-25 13:18:22 +0300

    ref

--- app/models/deposit.py
+++ app/models/deposit.py
@@ -14,9 +14,11 @@ class Deposit(Base):
     type = Column(Enum("credit", "debit", name="card_type"), nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     currency_id = Column(Integer, ForeignKey("currencies.id"), nullable=False)
+    card_id = Column(Integer, ForeignKey("cards.id"), nullable=True)
 
     user = relationship("User", back_populates="deposits")
     currency = relationship("Currency", back_populates="deposits")
+    card= relationship("Card", back_populates="deposits")
 
     @validates("amount")
     def validate_amount(self, key, v: float):
