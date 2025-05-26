ba31341531718368ad0e39d3cae78bf9c110f032
Martin <martin.kitukov@gmail.com>
2025-05-26 19:47:53 +0300

    withdrawal added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from .user import User
 from .card import Card
 from .contact import Contact
 from .deposit import Deposit
+from .withdrawal import Withdrawal
 from .transaction import Transaction
 from .category import Category
 from .recurring_transation import RecurringTransaction
