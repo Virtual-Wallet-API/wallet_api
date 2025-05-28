a2b8a23ba57e2b992d05941a9e4e9293b44a9438
fcgith <steliyan.slavov31@icloud.com>
2025-05-28 14:06:31 +0300

    requirements.txt

--- app/business/notification_service.py
+++ app/business/notification_service.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Dict
-
 from app.models import User
+import httpx
 
 
 class NotificationType(str, Enum):
--- app/config.py
+++ app/config.py
@@ -17,3 +17,8 @@ STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY",
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY",
                               "sk_test_51RT1Qx4CifUjVaUzRu8Nv8htWrmS91inHDpkEGdncJxnUzZuhbn1zCKtjkAOczQY2Hyak0y80dDX2yc4Erei3FXz00ihSFLEPj")
 STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET", "")  # Will be set when webhook is configured
+
+# Mailjet API config
+MAILJET_API_KEY = "a2c12e99dc915a4ab71a30d148740183"
+MAILJET_SECRET = "dded18c64b347adcffcb3b8ed07f2887"
+MAILJET_FROM_EMAIL = "steliyan.slavov31@icloud.com"
