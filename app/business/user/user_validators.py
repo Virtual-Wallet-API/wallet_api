01cca9a37188002028dcff5b9bd5fd09c6ddb6c5
fcgith <steliyan.slavov31@icloud.com>
2025-05-28 10:29:28 +0300

    validators

--- app/business/user/user_validators.py
+++ app/business/user/user_validators.py
@@ -8,40 +8,65 @@ from app.models import User
 not_exist = HTTPException(status_code=400, detail="User with these details does not exist")
 
 
-def check_user_with(field: str, value: str | int, db: Session) -> User | bool:
-    """
-    Check if a user exists in the database based on a specific field and value.
-    :param field: The field to search for the user (e.g., username, email).
-    :param value: The value corresponding to the field to be checked for the user.
-    :param db: The database session used to perform the user verification query.
-    :return: The `User` object if the user is found, otherwise returns `False`.
-    """
-    try:
-        return validate_user_exists_from(field, value, db)
-    except HTTPException:
+class UserValidators:
+    UniqueValidation: tuple = ("username", "email", "phone")
+
+    @staticmethod
+    def find_user_with(field: str, value: str | int, db: Session) -> User | bool:
+        """
+        Check if a user exists in the database based on a specific field and value.
+        :param field: The field to search for the user (e.g., username, email).
+        :param value: The value corresponding to the field to be checked for the user.
+        :param db: The database session used to perform the user verification query.
+        :return: The `User` object if the user is found, otherwise returns `False`.
+        """
+        try:
+            return UserValidators.find_user_with_or_raise_exception(field, value, db)
+        except HTTPException:
+            return False
+
+    @staticmethod
+    def find_user_with_or_raise_exception(field: str, value: str | int, db: Session, exc: Exception = None) -> User:
+        """
+        Returns a user matching the given field and value or raises exception 400 if not found.
+        :param field: The database field to search for.
+        :param value: The value to search for.
+        :param db: The database session.
+        :param exception: An exception to raise if the user is not found
+        :return: User object or raises an exception.
+        """
+        fields = {
+            "username": User.username,
+            "email": User.email,
+            "phone": User.phone_number,
+            "id": User.id
+        }
+
+        if field not in fields:
+            raise ValueError(f"Invalid field '{field}' provided for validate_user_exists_from function.")
+
+        user: Optional[User] = db.query(User).filter(fields[field] == value).first()
+        if user:
+            return user
+
+        if exc:
+            raise exc
+        raise not_exist
+
+    @staticmethod
+    def validate_unique_user_data(data: dict, db: Session) -> User | bool:
+        """
+        Validates user data and returns the user object if username, email or phone number is taken.
+        :param data: The user data to be validated.
+        :param db: The database session.
+        :return: User object if user with such credentials exists, otherwise returns False.
+        """
+        for field, value in data.items():
+            if field not in UserValidators.UniqueValidation:
+                continue
+
+            user = UserValidators.find_user_with(field, value, db)
+            if user:
+                return user
+
         return False
-
-
-def validate_user_exists_from(field: str, value: str | int, db: Session) -> User:
-    """
-    Returns a user matching the given field and value or raises exception 400 if not found.
-    :param field: The database field to search for.
-    :param value: The value to search for.
-    :param db: The database session.
-    :return: User object or raises an exception.
-    """
-    fields = {
-        "username": User.username,
-        "email": User.email,
-        "phone": User.phone_number,
-        "id": User.id
-    }
-
-    if field not in fields:
-        raise ValueError(f"Invalid field '{field}' provided for validate_user_exists_from function.")
-
-    user: Optional[User] = db.query(User).filter(fields[field] == value).first()
-    if user:
-        return user
-
-    raise not_exist
--- app/infrestructure/validators.py
+++ app/infrestructure/validators.py
@@ -1,36 +1,74 @@
 from fastapi import HTTPException
 
 
-def validate_username(v: str) -> str:
-    """
-        Check if a username is between 2 and 20 characters long
-    """
-    if len(v) < 2 or len(v) > 20:
-        raise HTTPException(status_code=400, detail="Username must be between 2 and 20 characters long")
+class DataValidators:
+    _UserValidators: dict  # Mapping user validators
 
-    return v
+    def __init__(self):
+        self._UserValidators = {
+            "username": self.validate_username,
+            "password": self.validate_password,
+            "phone_number": self.validate_phone_number,
+            "email": self.validate_email,
+        }
 
+    @property
+    def UserValidators(self):
+        return self._UserValidators
 
-def validate_password(v: str):
-    """
-        Check if a password is at least 8 characters long
-    """
-    if len(v) < 8:
-        raise HTTPException(status_code=400,
-                            detail="Password must be at least 8 characters long")
+    def validate_user_data(self, data: dict) -> dict:
+        """
+            Validate user data input - username, email, and phone number
+        """
+        returnData = {}
 
-    if not any(char.isdigit() for char in v) \
-            or not any(char.isupper() for char in v) \
-            or all(char.isalnum() for char in v):
-        raise HTTPException(status_code=400,
-                            detail="Password must contain at least one digit, one uppercase letter and one special character")
-    return v
+        for field, value in data:
+            if field in self._UserValidators:
+                returnData[field] = self._UserValidators[field](value)
 
+        return returnData
 
-def validate_phone_number(v: str):
-    """
-        Validate a phone number
-    """
-    if len(v) != 10:
-        raise HTTPException(status_code=400, detail="Phone number must be valid")
-    return v
+    @classmethod
+    def validate_username(cls, v: str) -> str:
+        """
+            Check if a username is between 2 and 20 characters long
+        """
+        if len(v) < 2 or len(v) > 20:
+            raise HTTPException(status_code=400, detail="Username must be between 2 and 20 characters long")
+
+        return v
+
+    @classmethod
+    def validate_password(cls, v: str) -> str:
+        """
+            Check if a password is at least 8 characters long
+        """
+        if len(v) < 8:
+            raise HTTPException(status_code=400,
+                                detail="Password must be at least 8 characters long")
+
+        if not any(char.isdigit() for char in v) \
+                or not any(char.isupper() for char in v) \
+                or all(char.isalnum() for char in v):
+            raise HTTPException(status_code=400,
+                                detail="Password must contain at least one digit, one uppercase letter and one special character")
+        return v
+
+    @classmethod
+    def validate_phone_number(cls, v: str) -> str:
+        """
+            Validate a phone number
+        """
+        if len(v) != 10:
+            raise HTTPException(status_code=400, detail="Phone number must be valid")
+        return v
+
+    @classmethod
+    def validate_email(cls, v: str) -> str:
+        """
+            Validate an email address
+            TODO: Add more robust email validation
+        """
+        if v.count("@") != 1 or "." not in v:
+            raise HTTPException(status_code=400, detail="Email address must be valid")
+        return v
