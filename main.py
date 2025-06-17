a84f56c9fe7e765194d8cbf8ba61b16e48942e68
fcgith <steliyan.slavov31@icloud.com>
2025-06-18 01:17:54 +0300

    Frontend cors fix

--- main.py
+++ main.py
@@ -51,7 +51,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:80", "http://127.0.0.1:3000", "http://127.0.0.1:8000",
+                   "http://localhost:80", "http://localhost:3000", "http://localhost:8000",
+                   "http://vwallet.ninja", "http://vwallet.ninja:80", "http://vwallet.ninja:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
